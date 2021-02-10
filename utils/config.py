70a6f3272e35dc250856cc2d8dd104f99cd7ad4b
vcokltfre <vcokltfre@gmail.com>
2021-02-10 04:44:41 +0000

    feat: add config util

---
+++ utils/config.py
@@ -0,0 +1,26 @@
+from yaml import safe_load
+from pathlib import Path
+
+class Loader:
+    def __init__(self):
+        self.cache = {}
+
+    def flush(self, guild: int = None):
+        if guild:
+            del self.cache[guild]
+            return
+        self.cache = {}
+
+    def load(self, guild: int) -> dict:
+        p = Path(f"./guilds/{guild}.yml")
+
+        if not p.exists(): return {}
+
+        with p.open(encoding="utf-8") as f:
+            data = safe_load(f)
+
+        self.cache[guild] = data
+
+    def reload(self, guild: int) -> dict:
+        self.flush(guild)
+        return self.load(guild)
