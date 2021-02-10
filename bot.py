4dc68284ce87d488f055349cc8de4d5bf1975190
vcokltfre <vcokltfre@gmail.com>
2021-02-10 04:49:08 +0000

    feat: add cfg bot attribute

--- bot.py
+++ bot.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 from os import environ as env
 from discord import Intents
 
+from utils.config import Loader
+
 load_dotenv(".env")
 
 bot = Bot(name="MessageSafe", command_prefix=env.get("PREFIX", "~"), intents=Intents.all())
+bot.cfg = Loader()
 bot.load_initial_cogs()
 
 bot.run(env["TOKEN"])
